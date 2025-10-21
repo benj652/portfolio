b9eaac065c8803dacd2ce13d362c43b2b33a7cce
benj652 <benjaminjaffe8@gmail.com>
2025-10-21 19:53:29 -0400

    fixing prod issues

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/portfolio/", 
+  base: "/", // Use root base path for preview
+  build: {
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]"
+      }
+    }
+  }
 });
 
